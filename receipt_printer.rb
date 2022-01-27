524c2640914254321b5bb8031aec049313b3611a
Shubham Porwall <sporwal@grepruby.io>
2022-01-27 18:50:26 +0530

    modified

--- receipt_printer.rb
+++ receipt_printer.rb
@@ -6,12 +6,14 @@ class ReceiptPrinter
   def run
     sales_tax = 0.0
     total = 0.0
+
     @products.each do |product|
       sales_tax += product[:tax]
       final_price = (product[:price] + product[:tax]).round(2) 
       total += final_price
       puts product[:quantity].to_s + ' ' + product[:name] + ': ' + final_price.to_s
     end
+    
     puts "Sales Tax: " + sales_tax.round(2).to_s
     puts "Total: " + total.to_s
   end
